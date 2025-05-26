f4d8181da350d7d67f693d1e7710250267103a2e
Rudranil <rudranilkhanra090@gmail.com>
2025-05-26 22:19:05 +0530

    Update capture_image.py
    FIle without integration

--- capture_image.py
+++ capture_image.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python3
+
 import cv2
-capture_s=cv2.vediocapture(0)
-rate,frame=capture_s.read()
+import time
+
+# Open the laptop camera (device 0)
+cap = cv2.VideoCapture(0)
+i=1
+while i<=5:
+    if not cap.isOpened():
+        print("Cannot access camera")
+        exit()
+    print("Get ready")
+    time.sleep(3)
+    ret, frame = cap.read()
+
+    if ret:
+        i+=1
+        k=(f"photo{i}.jpg")
+        
+        cv2.imwrite(k, frame)
+        print(f"Photo saved as photo{k}.jpg")
+        time.sleep(10)
+    else:
+        print("Failed to capture image")
+
+cap.release()
